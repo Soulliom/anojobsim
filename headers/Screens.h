308c51fb363355be39b2b1ebb82cf81350f7681b
Soulliom <gevorkbagratyan@gmail.com>
2019-10-19 14:36:42 -0700

    added hobby opt

--- headers/Screens.h
+++ headers/Screens.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include <conio.h>
+#include <stdlib.h>
+
 #include "Turn.h"
 
 class Screens {
--- headers/Turn.h
+++ headers/Turn.h
@@ -1,40 +1,58 @@
 #pragma once
 #include <iostream>
 #include <conio.h>
+#include <stdlib.h>
+
+/* Preproccessor Keystoke Buttons */
+#define ONE 49
+#define TWO 50
 
 class Turn {
 
 private:
+	// Job
+	bool jobBool = false;
+	int randNumOfJobFound = 0; //Jobs Signed for 
+	int randWage = 0; //Random wage
+	int actsUntilProm = 0;
+	int randNumOfProm = 0; //Random number of promotions
+	int randProm = 0; //Random mark promotion
+	int daysUntilFired = 0; //Days fired after not working
+	int promCount = 0;
+	int randJobMood = 0;
+
+	//Hobby
+	int hobbyStressR = 0;
+	bool isHobby = false;
+	std::string sHobby = "No Hobby Yet";
+
+	//General
+	int rando = 0; //rand variable
 
 public:
+
 	//Ingame
 	int numOfTurns = 1;
 	int numOfActions = 2;
-	int amOfCash = 1000;
-	int randStress;
+	int amOfMarks = 1000;
+	int randStress = 0;
 	int stress = 0;
 	std::string career = "Start a new Job";
 
 	//Settings
-	char char_name[20] = "You";
-	int turn_limit = 0;
+	char charName[20] = "You";
+	int turnLimit = 0;
 	int seed = 0;
-	long long mark_goal = 1000000000;
-	int stress_cap = 100;
+	long long markGoal = 1000000000;
+	int stressCap = 100;
 
 	//Functions
 	void InitSeed();
 	bool CheckTurn(bool check_over);
 	void Job();
 	void Work();
+	void StrsRelief();
 
 	//Job
-	int jobs;
-	int randNumOfJobFound;
-	int randNumOfProm;
-	int randWage;
 	int wage = 0;
-	int randPromPerc;
-	int ConsDaysUntilFired;
-
 };
